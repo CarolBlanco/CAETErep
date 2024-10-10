df7b8edd420bfe9081554762685b90bc7fa039a3
CarolBlanco <carolynablanco@gmail.com>
2024-10-10 15:07:08 -0300

    commit test 19

--- src/budget.f90
+++ src/budget.f90
@@ -404,6 +404,7 @@ contains
 
             germinated_seeds(ri) = nint(seed_bank(ri)*0.5) !!GERMINATION
             seed_bank(ri) = nint(seed_bank(ri) - germinated_seeds(ri)) !!UPDATE SEEDBANK
+            germinated_seeds(ri) = 0.0D0
          endif
          
          
