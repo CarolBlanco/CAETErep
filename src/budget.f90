5fc108f7658c8ce41538a5b86b23e87274be4660
CarolBlanco <carolynablanco@gmail.com>
2024-10-24 15:02:25 -0300

    teste8

--- src/budget.f90
+++ src/budget.f90
@@ -386,12 +386,10 @@ contains
 
          if (nppa(p) .gt. 0 .and. prec .ge. 60.0) then !!CAROL
 
-            print *, "Tamanho do banco de sementes do PLS n.", p, " antes da produção_na_budget:", seed_bank(ri)
-
             call repro(nppa(p), height_aux(ri), n_seed(ri), remaining_npp(p)) ! seed_bank(ri), new_seed_bank(ri)) ! ---> Usar height_aux(ri) ou height_aux(p) ???
             !seed_bank(ri) = seed_bank(ri) + n_seed(ri) !!UPDATE SEFEDBANK
             !seed_bank(ri) = new_seed_bank(ri)
-            print *, "Tamanho do banco de sementes após a chamada da produção_na_budget:", seed_bank(ri)
+            print *, "Tamanho do banco de sementes do PLS n.", p, " antes da produção_na_budget:", seed_bank(ri)
 
             nppa(p) = remaining_npp(p)
 
@@ -400,9 +398,9 @@ contains
                seed_bank_new(ri) = nint(seed_bank(ri) + n_seed(ri))
                seed_bank(ri) = seed_bank_new(ri)  ! Não altera se não houver produção
             !else
-            !   n_seed(ri) = 0
+               print *, "Tamanho do banco de sementes do PLS n.", p , " após a produção_na_budget:", seed_bank(ri)
             endif
-            print *, "Tamanho do banco de sementes após a produção_na_budget:", seed_bank(ri)
+            
 
          endif
          
@@ -410,7 +408,7 @@ contains
          if (seed_bank(ri) .gt. 0 .and. temp .ge. 23.0) then !CAROL
 
             germinated_seeds(ri) = nint(seed_bank(ri)*0.5) !!GERMINATION
-            print *, "***** Germinaram:", germinated_seeds(ri)
+            print *, "***** Germinaram:", germinated_seeds(ri), " sementes do PLS ", p
 
             seed_bank(ri) = nint(seed_bank(ri) - germinated_seeds(ri)) !!UPDATE SEEDBANK
             print *, "Tamanho do banco de sementes após a germinação:", seed_bank(ri)
